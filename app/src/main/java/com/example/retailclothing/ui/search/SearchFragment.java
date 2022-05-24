a5d6b71584201d38dc18bbae3067493232398eca
Mira79 <103221532+Mira79@users.noreply.github.com>
2022-05-24 18:59:08 -0400

    Update SearchFragment.java

--- app/src/main/java/com/example/retailclothing/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/retailclothing/ui/search/SearchFragment.java
@@ -17,8 +17,6 @@ public class SearchFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-       SearchViewModel searchViewModel =
-                new ViewModelProvider(this).get(SearchViewModel.class);
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
